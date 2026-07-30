1f1b4e652a4367955bbd4f4efad0389953a48618
CanhVeTaiGia <nghia120425@gmail.com>
2026-07-30 16:41:33 +0700

    style: touch layouts index.ts to force Vite HMR rebuild for custom negative margin timeline templates

--- my-video/src/compositions/layouts/index.ts
+++ my-video/src/compositions/layouts/index.ts
@@ -1,4 +1,4 @@
-import React from "react"; // trigger rebuild for circular_progress, ending, metrics, & custom restored and compressed chapter height timeline templates
+import React from "react"; // trigger rebuild for circular_progress, ending, metrics, & custom negative margin timeline templates
 import { LayoutProps } from "./LayoutTypes";
 import { TemplateLayout } from "./TemplateLayout";
 
