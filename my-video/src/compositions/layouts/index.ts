691e207e0e8ec450ae67e36d0831ba6b941891a6
CanhVeTaiGia <nghia120425@gmail.com>
2026-07-30 15:40:30 +0700

    style: touch layouts index.ts to force Vite HMR rebuild for custom import timeline templates

--- my-video/src/compositions/layouts/index.ts
+++ my-video/src/compositions/layouts/index.ts
@@ -1,4 +1,4 @@
-import React from "react"; // trigger rebuild for circular_progress, ending, metrics, & custom segregated timeline templates
+import React from "react"; // trigger rebuild for circular_progress, ending, metrics, & custom import timeline templates
 import { LayoutProps } from "./LayoutTypes";
 import { TemplateLayout } from "./TemplateLayout";
 
